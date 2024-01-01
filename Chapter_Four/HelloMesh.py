aa22933223075a029bad716fe290847b6da77ddb
MATHEW UPCHURCH <mupchurch777@gmail.com>
2024-01-01 14:58:33 -0800

    Chapter 4 Finish
    Finished Chapter 4 work

--- Chapter_Four/HelloMesh.py
+++ Chapter_Four/HelloMesh.py
@@ -1,7 +1,8 @@
 import pygame
-from Mesh3D import *
+from Cube import *
 from pygame.locals import *
 from OpenGL.GL import *
+from OpenGL.GLU import *
 
 pygame.init()
 
@@ -11,15 +12,21 @@ screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPEN
 
 pygame.display.set_caption('OpenGL in Python')
 
-mesh = Mesh3D()
+mesh = Cube()
 
 done = False
 white = pygame.Color(255, 255, 255)
+glOrtho(-1,1,1,-1,0.1, 100.0)
+# gluPerspective(30, (screen_width / screen_height), 0.1, 100.0)
+glTranslatef(0.0, 0.0, -3)
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glRotatef(5,1,0,1)
     mesh.draw()
     pygame.display.flip()
+    pygame.time.wait(100)
 pygame.quit()
